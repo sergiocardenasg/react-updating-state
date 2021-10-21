45d9cfeb27a1d20a26bb94c5c3a79c9e476524c8
sergiocardenasg <sergiocardenas@utexas.edu>
2021-10-20 21:42:26 -0600

    Revert "solved"
    This reverts commit 5f0bd9fd4e783e2f5b00a079c3d4529e5c0b0955.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ClickityClick from './components/ClickityClick';
 
 ReactDOM.render(
   <div>
-    <ClickityClick />
+    Mount Components Here
   </div>,
   document.getElementById('root')
 );
